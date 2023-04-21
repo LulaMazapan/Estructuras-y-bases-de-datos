e212f8df52b64119b5c77e9699a0f7c52a6a9e12
LulaMazapan <urielgrim24@gmail.com>
2023-04-20 20:01:40 -0600

    Practica2

---
+++ Practica2/Practica2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    int* Aux;
+    int *p[5];
+    int A[5];
+    A[0] = 8;
+    A[1] = 5;
+    A[2] = 7;
+    A[3] = 3;
+    A[4] = 9;
+
+    for (i = 0; i < 5; i++)
+    {
+        p[i] = &A[i];
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+
+        for (j = 0; j < 4; j++)
+        {
+            if (*p[j] > *p[j + 1])
+            {
+                Aux = p[j];
+
+                p[j] = p[j + 1];
+
+                p[j + 1] = Aux;
+            }
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << p[i] << "\t" << *p[i] << "\t" << &A[i] << "\t" << A[i] << "\t" << endl;
+    }
+
+    return 0;
+}
